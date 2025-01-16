8b9d0d2cb5d850b5da43eb3b9b70b09aa1b08c13
Tomasz Mantur <tomasz.mantur26@gmail.com>
2025-01-16 12:50:54 +0100

    Initial project commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    movies = [1, 2, 3, 4, 5, 6, 7, 8]
+    return render_template("homepage.html", movies=movies)
