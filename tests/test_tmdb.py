5032049c86320aef12018410299ab21c6a28083e
Tomasz Mantur <tomasz.mantur26@gmail.com>
2025-02-17 23:07:25 +0100

    add monkeypatch tests

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -1,3 +1,5 @@
+import pytest
+from unittest.mock import Mock
 import tmdb_client
 import requests
 
@@ -9,6 +11,29 @@ def test_get_poster_url_uses_default_size():
    poster_url = tmdb_client.get_poster_url(poster_api_path=poster_api_path)
    # Porównanie wyników
    assert expected_default_size in poster_url
+
+def test_call_tmdb_api_gives_proper_result(monkeypatch):
+    # Lista, którą będzie zwracać przysłonięte "zapytanie do API"
+    mock_movies_list = ['Movie 1', 'Movie 2']
+
+    requests_mock = Mock()
+    # Wynik wywołania zapytania do API
+    response = requests_mock.return_value
+    # Przysłaniamy wynik wywołania metody .json()
+    response.json.return_value = mock_movies_list
+    monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+
+    movies_list = tmdb_client.call_tmdb_api(endpoint="tv/airing_today")
+    assert movies_list == mock_movies_list
+
+def test_get_single_movie_gives_proper_result(monkeypatch):
+    mock_movie_data = {"id": 1234, "title": "Mocked Movie 2"}
+
+    call_tmdb_api_mock = Mock(return_value=mock_movie_data)
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", call_tmdb_api_mock)
+
+    result = tmdb_client.get_single_movie(123)
+    assert result == mock_movie_data
 
 def get_movies_list(list_type):
    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
