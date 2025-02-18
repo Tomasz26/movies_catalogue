e9d19a7048e18abb7516378f0a3878e773b1632b
Tomasz Mantur <tomasz.mantur26@gmail.com>
2025-02-18 20:47:12 +0100

    monkeypatch tests add

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -26,23 +26,37 @@ def test_call_tmdb_api_gives_proper_result(monkeypatch):
     movies_list = tmdb_client.call_tmdb_api(endpoint="tv/airing_today")
     assert movies_list == mock_movies_list
 
+def test_call_tmdb_api_handles_http_error(monkeypatch):
+    response_mock = Mock()
+    response_mock.raise_for_status.side_effect = requests.exceptions.HTTPError("Mocked HTTP error")
+
+    mock_requests_get = Mock(return_value = response_mock)
+    monkeypatch.setattr("tmdb_client.requests.get", mock_requests_get)
+
+    with pytest.raises(requests.exceptions.HTTPError, match="Mocked HTTP error"):
+        tmdb_client.call_tmdb_api("movie/popular")
+
 def test_get_single_movie_gives_proper_result(monkeypatch):
     mock_movie_data = {"id": 1234, "title": "Mocked Movie 2"}
 
     call_tmdb_api_mock = Mock(return_value=mock_movie_data)
     monkeypatch.setattr("tmdb_client.call_tmdb_api", call_tmdb_api_mock)
 
-    result = tmdb_client.get_single_movie(123)
+    result = tmdb_client.get_single_movie(1234)
     assert result == mock_movie_data
 
-def get_movies_list(list_type):
-   endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-   headers = {
-       "Authorization": f"Bearer {API_TOKEN}"
-   }
-   response = requests.get(endpoint, headers=headers)
-   response.raise_for_status()
-   return response.json()
+    call_tmdb_api_mock.assert_called_once_with("movie/1234")
+
+def test_get_movie_credits_gives_proper_result(monkeypatch):
+    mock_credits_data = {"name": "Peter Parker", "character": "Spiderman"}
+
+    call_tmdb_api_mock = Mock(return_value=mock_credits_data)
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", call_tmdb_api_mock)
+
+    result = tmdb_client.get_movie_credits(1234)
+    assert result == mock_credits_data
+
+    call_tmdb_api_mock.assert_called_once_with("movie/1234/credits")
 
 def test_get_movies_type_popular():
    movies_list = tmdb_client.get_movies(type="popular")
